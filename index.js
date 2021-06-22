4caa15bb88b104c78c056bde655eab71c2655331
yashjamkar <44546213+yashjamkar@users.noreply.github.com>
2021-06-22 13:05:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
@@ -13,6 +14,7 @@ for (var i = 0; i < numberOfDrums; i++) {
 
 document.addEventListener("keypress", function(event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 })
 
 function makeSound(key){
@@ -57,5 +59,16 @@ function makeSound(key){
 
   }
 
+}
+
+//Button Animation 
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+   activeButton.classList.add("pressed");
+
+   setTimeout(function (){
+     activeButton.classList.remove("pressed");
+   } , 100);
 }
 
